3b0b82ccdee39dacf190d719619c0b1b69512465
maqss <maksymgulko@gmail.com>
2024-07-14 20:31:26 +0200

    finish the 5th task

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,14 @@
+const colorBtn = document.querySelector(".change-color");
+
+colorBtn.addEventListener("click", handleColorChange);
+
+function handleColorChange(event) {
+  const bgColor = document.querySelector("body");
+  const spanColor = document.querySelector(".color");
+  bgColor.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
